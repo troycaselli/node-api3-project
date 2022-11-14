da27012fb54f823524d0f155544c72515d7b4d0d
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-14 12:58:49 -0800

    set up server port

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// require your server and launch it
+const server = require('./api/server');
+
+const port = 8000;
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
